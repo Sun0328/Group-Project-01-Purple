2c107f148129ff88a19e73846fe0998991d5667e
Annie Mu <smu815@UoA.auckland.ac.nz>
2023-05-19 15:35:07 +1200

    Update databse and DAO functions

--- modules/test-dao.js
+++ modules/test-dao.js
@@ -1,55 +1,54 @@
 const SQL = require("sql-template-strings");
 const dbPromise = require("./database.js");
 
-async function createTestData(testData) {
+async function createNewUser(user) {
     const db = await dbPromise;
 
-    const result = await db.run(SQL`
-        insert into test (stuff) values(${testData.stuff})`);
-
-    testData.id = result.lastID;
+    return await db.run(SQL`
+        insert into user (id, username, password, fname, lname, year, month, day, salt, profile, avatar, subscriber) values
+        (${user.id, user.username, user.password, user.fname, user.lname, user.year, user.month, user.day, user.salt, user.profile, user.avatar, user.subscriber})`);
 }
 
-async function retrieveTestDataById(id) {
+async function retrieveDataById(id, table) {
     const db = await dbPromise;
 
     const testData = await db.get(SQL`
-        select * from test
+        select * from ${table}
         where id = ${id}`);
 
     return testData;
 }
 
-async function retrieveAllTestData() {
+async function retrieveAllData(table) {
     const db = await dbPromise;
 
-    const allTestData = await db.all(SQL`select * from test`);
+    const allTestData = await db.all(SQL`select * from ${table}`);
 
     return allTestData;
 }
 
-async function updateTestData(testData) {
+async function updateData(data, table, column) {
     const db = await dbPromise;
 
     return await db.run(SQL`
-        update test
-        set stuff = ${testData.stuff}
-        where id = ${testData.id}`);
+        update ${table}
+        set ${column} = ${data.column}
+        where id = ${data.id}`);
 }
 
-async function deleteTestData(id) {
+async function deleteData(id, table) {
     const db = await dbPromise;
 
     return await db.run(SQL`
-        delete from test
+        delete from ${table}
         where id = ${id}`);
 }
 
 // Export functions.
 module.exports = {
-    createTestData,
-    retrieveTestDataById,
-    retrieveAllTestData,
-    updateTestData,
-    deleteTestData
+    createNewUser,
+    retrieveDataById,
+    retrieveAllData,
+    updateData,
+    deleteData
 };
